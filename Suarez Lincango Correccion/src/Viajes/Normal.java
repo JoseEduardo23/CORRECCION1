b46c90788a2dfaded849132f8ae726189d08b7f5
Michelle Suarez <michelle.suarez@epn.edu.ec>
2024-06-20 08:19:35 -0500

    Creacion de atributos y metodos en las clases Servicio y Normal

--- Suarez Lincango Correccion/src/Viajes/Normal.java
+++ Suarez Lincango Correccion/src/Viajes/Normal.java
@@ -1,4 +1,41 @@
 package Viajes;
 
-public class Normal {
+public class Normal extends Servicio{
+
+    double precioFinal;
+
+    public Normal(String codigoTicket, String fechaTicket, String nombre, String cedula, int edad, String telefono, String origen, String destino, double precio, String asiento, boolean maleta, boolean maletaextra) {
+        super(codigoTicket, fechaTicket, nombre, cedula, edad, telefono, origen, destino, precio, asiento, maleta, maletaextra);
+    }
+
+    public Normal(String codigoTicket, String fechaTicket, String nombre, String cedula, int edad, String telefono, String origen, String destino, double precio, String asiento, boolean maleta, boolean maletaextra, double precioFinal) {
+        super(codigoTicket, fechaTicket, nombre, cedula, edad, telefono, origen, destino, precio, asiento, maleta, maletaextra);
+        this.precioFinal = precioFinal;
+    }
+
+    public double getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public void setPrecioFinal(double precioFinal) {
+        this.precioFinal = precioFinal;
+    }
+
+    @Override
+    public void mostrarTicket() {
+        System.out.println("Ticket: "+codigoTicket);
+        System.out.println("Fecha: "+fechaTicket);
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Cedula: "+cedula);
+        System.out.println("Edad: "+edad);
+        System.out.println("Telefono: "+telefono);
+        System.out.println("Origen: "+origen);
+        System.out.println("Destino: "+destino);
+        System.out.println("Precio: "+precio);
+        System.out.println("Asiento: "+asiento);
+        System.out.println("Maleta: "+maleta);
+        System.out.println("Maletaextra: "+maletaextra);
+        System.out.println("PrecioFinal: "+precioFinal);
+
+    }
 }
--- Suarez Lincango Correccion/src/Viajes/Servicio.java
+++ Suarez Lincango Correccion/src/Viajes/Servicio.java
@@ -1,4 +1,43 @@
 package Viajes;
 
-public class Servicio {
+public class Servicio extends Ruta {
+    String asiento;
+    boolean maleta;
+    boolean maletaextra;
+
+    public Servicio(String codigoTicket, String fechaTicket, String nombre, String cedula, int edad, String telefono, String origen, String destino, double precio, String asiento, boolean maleta, boolean maletaextra) {
+        super(codigoTicket, fechaTicket, nombre, cedula, edad, telefono, origen, destino, precio);
+        this.asiento = asiento;
+        this.maleta = maleta;
+        this.maletaextra = maletaextra;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public boolean isMaleta() {
+        return maleta;
+    }
+
+    public void setMaleta(boolean maleta) {
+        this.maleta = maleta;
+    }
+
+    public boolean isMaletaextra() {
+        return maletaextra;
+    }
+
+    public void setMaletaextra(boolean maletaextra) {
+        this.maletaextra = maletaextra;
+    }
+
+    public void mostrarTicket() {
+    //ya veremos
+    }
 }
+
