681f462fa0fec2124ffa25e799c1c5a5836554ab
Eduardo <joselincango73@gmail.com>
2024-06-20 08:18:12 -0500

    Clases ServicioVIP Y VIP, clases y atributos JL

--- Suarez Lincango Correccion/src/Viajes/ServicioVIP.java
+++ Suarez Lincango Correccion/src/Viajes/ServicioVIP.java
@@ -1,4 +1,85 @@
 package Viajes;
 
-public class ServicioVIP {
+public class ServicioVIP extends Ruta {
+    String asiento;
+    boolean maleta;
+    boolean maletaextra;
+    boolean television;
+    boolean internet;
+    String sevicio1;
+    String sevicio2;
+
+    public ServicioVIP(String codigoTicket, String fechaTicket, String nombre, String cedula, int edad, String telefono, String origen, String destino, double precio, String asiento, boolean maleta, boolean maletaextra) {
+    }
+
+    public ServicioVIP(String codigoTicket, String fechaTicket, String nombre, String cedula, int edad, String telefono, String origen, String destino, double precio, String asiento, boolean maleta, boolean maletaextra, boolean television, boolean internet, String sevicio1, String sevicio2) {
+        super(codigoTicket, fechaTicket, nombre, cedula, edad, telefono, origen, destino, precio);
+        this.asiento = asiento;
+        this.maleta = maleta;
+        this.maletaextra = maletaextra;
+        this.television = television;
+        this.internet = internet;
+        this.sevicio1 = sevicio1;
+        this.sevicio2 = sevicio2;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public boolean isMaleta() {
+        return maleta;
+    }
+
+    public void setMaleta(boolean maleta) {
+        this.maleta = maleta;
+    }
+
+    public boolean isMaletaextra() {
+        return maletaextra;
+    }
+
+    public void setMaletaextra(boolean maletaextra) {
+        this.maletaextra = maletaextra;
+    }
+
+    public boolean isTelevision() {
+        return television;
+    }
+
+    public void setTelevision(boolean television) {
+        this.television = television;
+    }
+
+    public boolean isInternet() {
+        return internet;
+    }
+
+    public void setInternet(boolean internet) {
+        this.internet = internet;
+    }
+
+    public String getSevicio1() {
+        return sevicio1;
+    }
+
+    public void setSevicio1(String sevicio1) {
+        this.sevicio1 = sevicio1;
+    }
+
+    public String getSevicio2() {
+        return sevicio2;
+    }
+
+    public void setSevicio2(String sevicio2) {
+        this.sevicio2 = sevicio2;
+    }
+
+    public void mostrarTicket() {
+        //metodoidk
+    }
 }
--- Suarez Lincango Correccion/src/Viajes/VIP.java
+++ Suarez Lincango Correccion/src/Viajes/VIP.java
@@ -1,4 +1,37 @@
 package Viajes;
 
-public class VIP {
+public class VIP extends ServicioVIP{
+
+    double precioFinal;
+
+    public VIP(String codigoTicket, String fechaTicket, String nombre, String cedula, int edad, String telefono, String origen, String destino, double precio, String asiento, boolean maleta, boolean maletaextra, boolean television, boolean internet, String sevicio1, String sevicio2, double precioFinal) {
+        super(codigoTicket, fechaTicket, nombre, cedula, edad, telefono, origen, destino, precio, asiento, maleta, maletaextra, television, internet, sevicio1, sevicio2);
+        this.precioFinal = precioFinal;
+    }
+    public double getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public void setPrecioFinal(double precioFinal) {
+        this.precioFinal = precioFinal;
+    }
+    @Override
+    public void mostrarTicket() {
+        System.out.println("Ticket: "+codigoTicket);
+        System.out.println("Fecha: "+fechaTicket);
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Cedula: "+cedula);
+        System.out.println("Edad: "+edad);
+        System.out.println("Telefono: "+telefono);
+        System.out.println("Origen: "+origen);
+        System.out.println("Destino: "+destino);
+        System.out.println("Precio: "+precio);
+        System.out.println("Asiento: "+asiento);
+        System.out.println("Maleta: "+maleta);
+        System.out.println("Maletaextra: "+maletaextra);
+        System.out.println("Television: "+television);
+        System.out.println("Internet: "+internet);
+        System.out.println("Sevicio1: "+sevicio1);
+        System.out.println("Sevicio2: "+sevicio2);
+    }
 }
